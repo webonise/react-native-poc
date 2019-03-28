4f50b8cb1d92d19ed2a6f348323bb5ee6357f569
ameyawebonise <ameya.kulkarni@weboniselab.com>
2019-03-28 18:54:21 +0530

    Clobbered up a basic api call

--- EXPO/screens/LinksScreen.js
+++ EXPO/screens/LinksScreen.js
@@ -1,21 +1,68 @@
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
+import { FlatList, StyleSheet,Text,View } from 'react-native';
 import { ExpoLinksView } from '@expo/samples';
 
 export default class LinksScreen extends React.Component {
+
+  constructor(props) {
+    super(props);
+    //this.getUserInfo = getUserInfo.bind(this);
+    this.state = {
+      userList: [],
+      page: 0,
+      per_page: 0,
+      total: 0,
+      total_pages: 0
+    };
+  }
+
   static navigationOptions = {
     title: 'Links',
   };
 
+
   render() {
     return (
-      <ScrollView style={styles.container}>
-        {/* Go ahead and delete ExpoLinksView and replace it with your
-           * content, we just wanted to provide you with some helpful links */}
-        <ExpoLinksView />
-      </ScrollView>
+      <View style={{flex: 1, paddingTop:20}}>
+        <FlatList
+          data={this.state.userList}
+          renderItem={({item}) => <Text>{item.first_name}, {item.last_name}</Text>}
+          keyExtractor={(item, index) => index.toString()}
+        />
+        <Text>Page number - {this.state.page}</Text>
+        <Text>Number of records per page - { this.state.per_page}</Text>
+        <Text>Total - {this.state.total_pages}</Text>
+        <Text>Total Items - {this.state.total}</Text>
+      </View>
+    //);
     );
   }
+
+  componentWillMount(){
+    this.getUserInfo(0)
+  }
+
+    getUserInfo (page){
+    console.log("getting user info")
+    const URL = `https://reqres.in/api/users`;
+    return fetch(URL)
+            .then((res) =>res.json())
+            .then(data =>{
+              this.setState({
+                per_page:data.per_page,
+                total:data.total,
+                total_pages:data.total_pages,
+                page:data.page,
+                userList:data.data
+              })
+              console.log(data.per_page)
+            })
+            .catch(function(error) {
+                console.log('There has been a problem with your fetch operation: ' + error);
+                  // ADD THIS THROW error
+                throw error;
+    });
+    }
 }
 
 const styles = StyleSheet.create({
