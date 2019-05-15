8b0adf9122d319a96d940e2a0203e3a64161a72e
Rakesh Medpalli <rakesh.medpalli@weboniselab.com>
2019-05-15 18:19:41 +0530

    AppIcon and Splash sceen added

--- EXPO/App.js
+++ EXPO/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
-import { AppLoading, Asset, Font, Icon } from 'expo';
+import { Platform, StatusBar, StyleSheet, View,Image } from 'react-native';
+import { AppLoading,SplashScreen, Asset, Font, Icon } from 'expo';
 import AppNavigator from './navigation/AppNavigator';
-
 export default class App extends React.Component {
   state = {
     isLoadingComplete: false,
@@ -60,3 +59,73 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
   },
 });
+
+/*
+export default class App extends React.Component {
+  state = {
+    isSplashReady: false,
+    isAppReady: false,
+  };
+
+  render() {
+    if (!this.state.isSplashReady) {
+      return (
+        <AppLoading
+          startAsync={this._cacheSplashResourcesAsync}
+          onFinish={() => this.setState({ isSplashReady: true })}
+          onError={console.warn}
+          autoHideSplash={false}
+        />
+      );
+    }
+
+    if (!this.state.isAppReady) {
+      return (
+        <View style={{ flex: 1 }}>
+          <Image style= {{width: 100,height: 100}}
+            source={require('./assets/images/weboniseIcon.png')} 
+           // source = {{uri: 'http://www.clipartsmania.com/gif/animals_gif/cartoon_birds_blue_flying_animation_clipart.gif'}}
+            onLoad={this._cacheResourcesAsync}
+          />
+        </View>
+      );
+    }
+/*
+ <Image style= {{width: 100,height: 100, borderRadius: 100/2, margin:5}}
+               source = {{uri: item.avatar}}
+         /> */
+   /* return (
+      <View style={styles.container}>
+        {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
+        <AppNavigator />
+      </View>
+    );    
+  }
+
+  _cacheSplashResourcesAsync = async () => {
+    const gif = require('./assets/images/weboniseIcon.png');
+    return Asset.fromModule(gif).downloadAsync()
+  }
+
+  _cacheResourcesAsync = async () => {
+    SplashScreen.hide();
+    const images = [
+      require('./assets/images/webonise-logo.png'),
+      require('./assets/images/robot-dev.png'),
+    ];
+
+    const cacheImages = images.map((image) => {
+      return Asset.fromModule(image).downloadAsync();
+    });
+
+    await Promise.all(cacheImages);
+    this.setState({ isAppReady: true });
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#0000',
+  },
+});  */
